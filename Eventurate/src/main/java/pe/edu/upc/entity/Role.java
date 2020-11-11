7a338a7fdca21763233b9ef64a517a06d693b3fc
Yopsquienmas <brianjsolis2000@gmail.com>
2020-11-11 10:21:11 -0500

    Entity Role

---
+++ Eventurate/src/main/java/pe/edu/upc/entity/Role.java
@@ -0,0 +1,54 @@
+package pe.edu.upc.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="roles")
+public class Role implements Serializable{
+
+	private static final long serialVersionUID=1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idRole;
+	
+	@Column(name="nameRole")
+	private String nameRole;
+
+	public Role() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Role(int idRole, String nameRole) {
+		super();
+		this.idRole = idRole;
+		this.nameRole = nameRole;
+	}
+
+	public int getIdRole() {
+		return idRole;
+	}
+
+	public void setIdRole(int idRole) {
+		this.idRole = idRole;
+	}
+
+	public String getNameRole() {
+		return nameRole;
+	}
+
+	public void setNameRole(String nameRole) {
+		this.nameRole = nameRole;
+	}
+
+	
+	
+}
