4ef6e13e60524456486a2c0015e3d6eec7820e79
jeanmarko2703 <jeanmarkoaguirre@gmail.com>
2020-11-10 22:53:03 -0500

    creación del proyecto

---
+++ Eventurate/src/main/java/pe/edu/upc/EventurateApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventurateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventurateApplication.class, args);
+	}
+
+}
